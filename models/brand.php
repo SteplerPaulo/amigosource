cec62a5d37a12ee51086c9338b40bf79cc749f90
Paulo <paulobiscocho@gmail.com>
2015-03-09 21:40:18 +0800

    Brand Model Init Commit

---
+++ models/brand.php
@@ -0,0 +1,23 @@
+<?php
+class Brand extends AppModel {
+	var $name = 'Brand';
+	var $displayField = 'name';
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+	var $hasMany = array(
+		'Product' => array(
+			'className' => 'Product',
+			'foreignKey' => 'brand_id',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		)
+	);
+
+}
