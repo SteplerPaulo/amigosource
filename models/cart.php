cab76191788073cdf798d2ea46956123cd3c9b12
Paulo <paulobiscocho@gmail.com>
2015-03-09 21:40:30 +0800

    Cart Model Init Commit

---
+++ models/cart.php
@@ -0,0 +1,16 @@
+<?php
+class Cart extends AppModel {
+	var $name = 'Cart';
+	var $displayField = 'name';
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+	var $belongsTo = array(
+		'Product' => array(
+			'className' => 'Product',
+			'foreignKey' => 'product_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+}
