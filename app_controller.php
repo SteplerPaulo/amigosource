723b26e6da2292257d4b9d04c0e76efdfcdd31f1
Paulo <paulobiscocho@gmail.com>
2015-03-10 23:43:29 +0800

    App Controller Updates
    Added New Functions:
    Admin Switch
    Admin Editable
    Tags Changer Function

--- app_controller.php
+++ app_controller.php
@@ -31,4 +31,57 @@
  * @subpackage    cake.app
  */
 class AppController extends Controller {
+
+	/////////////////////////////////////////////////////////////
+	public function admin_switch($field = null, $id = null) {
+        $this->autoRender = false;
+        $model = $this->modelClass;
+        if ($this->$model && $field && $id) {
+            $field = $this->$model->escapeField($field);
+            return $this->$model->updateAll(array($field => '1 -' . $field), array($this->$model->escapeField() => $id));
+        }
+        if(!$this->RequestHandler->isAjax()) {
+            return $this->redirect($this->referer());
+        }
+    }
+	
+	
+	////////////////////////////////////////////////////////////
+    public function admin_editable() {
+        $model = $this->modelClass;
+
+        $id = trim($this->params['form']['pk']);
+        $field = trim($this->params['form']['name']);
+        $value = trim($this->params['form']['value']);
+
+        $data[$model]['id'] = $id;
+        $data[$model][$field] = $value;
+        $this->$model->save($data, false);
+
+        $this->autoRender = false;
+
+    }
+
+	////////////////////////////////////////////////////////////
+    public function admin_tagschanger() {
+
+        $value = '';
+
+        asort($this->params['form']['value']);
+
+        foreach ($this->params['form']['value'] as $k => $v) {
+            $value .= $v . ', ';
+        }
+
+        $value = trim($value);
+        $value = rtrim($value, ',');
+
+
+        $this->Product->id = $this->params['form']['pk'];
+        $s = $this->Product->saveField('tags', $value, false);
+
+        $this->autoRender = false;
+
+    }
+	////////////////////////////////////////////////////////////
 }
