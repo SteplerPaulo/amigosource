fa086d02e9eaaede57ca785f70a9d0d6cd69ecbd
Paulo <paulobiscocho@gmail.com>
2015-03-17 18:29:45 +0800

    Add Event Handler for Monetary Currency & Country

--- webroot/js/admin.js
+++ webroot/js/admin.js
@@ -15,11 +15,50 @@ $(document).ready(function() {
 		return false;
 	});
 	
-	//User-Account Product Details
+	//TOOL TIP INITIALIZATION
+	$('[data-toggle="tooltip"]').tooltip();
+	
+	/**************USER-ACCOUNT PRODUCT DETAILS**************/
+	//GENERAL CATEGORY
 	$(".general-category").change(function(){
 		$('.general-category-classification option:not(:first)').remove();
 		$('.general-category-classification').append($('.classification-category option[parent-id="'+$(this).val()+'"]').clone())
 	});
 	
-	$('[data-toggle="tooltip"]').tooltip()
+	
+	/***************CREDENTIAL-PROFILE*************/
+	//MONETARY CURRENCY EVENT HANDLER
+	$('.monetary-currency').change(function(){
+		var row  = $(this).parents('tr:first'); 
+		var symbol = $(this).find('option:selected').attr('symbol');
+		if(symbol){
+			$(row).find('.currency_symbol').html(symbol);
+		}else{
+			$(row).find('.currency_symbol').html('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;');
+		}
+		
+	});
+	
+	/****************SUPPLIER-MEMBER-DETAILS****************/
+	//COUNTRY EVENT HANDLER
+	$('#CountryId').change(function(){
+		toggle_curr_province_form_input($(this).val());
+	});
+	
+	
+	//ALLOW TO SET PROVINCE ON LOAD
+	toggle_curr_province_form_input($('#CountryId').val());
+	
+	//TOGGLE CURRENT PROVINCE FORM INPUT
+	function toggle_curr_province_form_input(curr_country_id){
+		if(curr_country_id == 175){
+			$('#ProvinceDropDown').removeAttr('disabled').parents('div:first').show();
+			$('#ProvinceText').attr('disabled','disabled').parents('div:first').hide();
+			$('#ProvinceText').val('');
+		}else{
+			$('#ProvinceText').removeAttr('disabled').parents('div:first').show();
+			$('#ProvinceDropDown').attr('disabled','disabled').parents('div:first').hide();
+			$('#ProvinceDropDown').val('');
+		}
+	}
 });
