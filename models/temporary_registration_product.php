0b821a9350e3a947232d7e88310a7410528938e7
Paulo <paulobiscocho>
2015-05-16 22:34:13 +0800

    Example Photos Used While Testing MEIO

---
+++ models/temporary_registration_product.php
@@ -0,0 +1,55 @@
+<?php
+class TemporaryRegistrationProduct extends AppModel {
+	var $name = 'TemporaryRegistrationProduct';
+	var $displayField = 'name';
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+	/*
+	var $belongsTo = array(
+		'TemporaryRegistration' => array(
+			'className' => 'TemporaryRegistration',
+			'foreignKey' => 'temporary_registration_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		),
+		'GeneralCategory' => array(
+			'className' => 'GeneralCategory',
+			'foreignKey' => 'general_category_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		),
+		'Classification' => array(
+			'className' => 'Classification',
+			'foreignKey' => 'classification_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		),
+		'MonetaryCurrency' => array(
+			'className' => 'MonetaryCurrency',
+			'foreignKey' => 'monetary_currency_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+	*/
+	
+	var $actsAs = array(
+		'MeioUpload' => array(
+			'image' => array(
+				'dir' => 'img{DS}uploads{DS}product-images',
+				'create_directory' => false,
+				'allowed_mime' => array('image/jpeg', 'image/pjpeg', 'image/png'),
+				'allowed_ext' => array('.jpg', '.jpeg', '.png'),
+				'zoomCrop' => true,
+				'thumbsizes' => array(
+					'normal' => array('width' => 400, 'height' => 300),
+					'small' => array('width' => 80, 'height' => 80,'maxDimension' => '', 'thumbnailQuality' => 100, 'zoomCrop' => true),
+				),
+				'default' => 'default.jpg'
+			)
+		)
+	);
+}
