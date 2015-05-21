49fec41202ada665df82eb902b5e02db5d5c7186
Garry M. Perez <perez.garrym@gmail.com>
2015-05-21 15:39:14 +0800

    remove comments

--- webroot/js/biz/user-approval.js
+++ webroot/js/biz/user-approval.js
@@ -1,16 +1,5 @@
 var BASE_URL ='/'+window.location.pathname.split('/')[1]+'/';
 $(document).ready(function(){
-	/*$('.approve').on('click',function(){
-		$.ajax({
-			url:BASE_URL+'temporary_registrations/test_file',
-			dataType:'json',
-			data:{},
-			type:'post',
-			success:function(FormReturn){
-				console.log(FormReturn);
-			}
-		}); 
-	});*/
 	 $('.approve').on('click',function(){
 	  $.ajax({
            type: "POST",
