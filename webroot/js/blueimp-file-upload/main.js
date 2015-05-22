d3ecdfc8ee2b18e6a591ac16e297892fca084791
Paulo <paulobiscocho>
2015-05-22 17:52:37 +0800

    Blue imp File Upload Main JS Adjusted For Further Use

--- webroot/js/blueimp-file-upload/main.js
+++ webroot/js/blueimp-file-upload/main.js
@@ -15,14 +15,14 @@ $(function () {
     'use strict';
 
     // Initialize the jQuery File Upload widget:
-    $('#fileupload').fileupload({
+    $('.fileupload').fileupload({
         // Uncomment the following to send cross-domain cookies:
         xhrFields: {withCredentials: true},
         url: 'server'
     });
 
     // Enable iframe cross-domain access via redirect option:
-    $('#fileupload').fileupload(
+    $('.fileupload').fileupload(
         'option',
         'redirect',
 		window.location.href.replace(
@@ -33,13 +33,13 @@ $(function () {
 
 
 	// Load existing files:
-	$('#fileupload').addClass('fileupload-processing');
+	$('.fileupload').addClass('fileupload-processing');
 	$.ajax({
 		// Uncomment the following to send cross-domain cookies:
-		//xhrFields: {withCredentials: true},
-		url: $('#fileupload').fileupload('option', 'url'),
+		xhrFields: {withCredentials: true},
+		url: $('.fileupload').fileupload('option', 'url'),
 		dataType: 'json',
-		context: $('#fileupload')[0]
+		context: $('.fileupload')[0],
 	}).always(function () {
 		$(this).removeClass('fileupload-processing');
 	}).done(function (result) {
