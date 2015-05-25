7d096b4990f77f7ed8596b1622f4f7ecdc67f631
daveil <arroyo.daveil@gmail.com>
2015-05-25 17:29:49 +0800

    Map pictures to temp product

--- models/picture.php
+++ models/picture.php
@@ -2,4 +2,13 @@
 class Picture extends AppModel {
 	var $name = 'Picture';
 	var $displayField = 'name';
+	var $belongsTo = array(
+		'TemporaryRegistrationProduct' => array(
+			'className' => 'TemporaryRegistrationProduct',
+			'foreignKey' => 'tmp_product_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
 }
