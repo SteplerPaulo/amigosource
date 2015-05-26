6941f8bb15812de25a42fb4b38f91a45e15bf86d
Paulo <paulobiscocho>
2015-05-26 12:02:53 +0800

    RE ADJUST UPLOAD MAIN JS

--- webroot/js/blueimp-file-upload/main.js
+++ webroot/js/blueimp-file-upload/main.js
@@ -20,7 +20,7 @@ $(function () {
         xhrFields: {withCredentials: true},
         url: 'server'
     });
-
+		
     // Enable iframe cross-domain access via redirect option:
     $('.fileupload').fileupload(
         'option',
@@ -31,24 +31,18 @@ $(function () {
         )
     );
 
-
 	// Load existing files:
 	$('.fileupload').addClass('fileupload-processing');
-	
-	//$('.add-product-image').click(function(){
-		$.ajax({
-			// Uncomment the following to send cross-domain cookies:
-			xhrFields: {withCredentials: true},
-			url: $('.fileupload').fileupload('option', 'url'),
-			dataType: 'json',
-			context: $('.fileupload')[0],
-		}).always(function () {
-			$(this).removeClass('fileupload-processing');
-		}).done(function (result) {
-			$(this).fileupload('option', 'done').call(this, $.Event('done'), {result: result});
-		});
-	//});
-	
-    
 
+	$.ajax({
+		// Uncomment the following to send cross-domain cookies:
+		xhrFields: {withCredentials: true},
+		url: $('.fileupload').fileupload('option', 'url'),
+		dataType: 'json',
+		context: $('.fileupload')[0],
+	}).always(function () {
+		$(this).removeClass('fileupload-processing');
+	}).done(function (result) {
+		$(this).fileupload('option', 'done').call(this, $.Event('done'), {result: result});
+	});
 });
