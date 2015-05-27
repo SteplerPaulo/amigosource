e1671b97f28f94ebbfce587dc2c6c4f20906fd8f
Paulo <paulobiscocho@gmail.com>
2015-05-27 12:12:37 +0800

    Product JS Updated

--- webroot/js/biz/registration/products.js
+++ webroot/js/biz/registration/products.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
 	var BASEURL = '/'+window.location.pathname.split('/')[1]+'/';
-	
+	var ROW;
 	set_product_table();
 
 	//ADD PRODUCT BUTTON HANDLER
 	$('#AddProductButton').click(function(){	
-		console.log('wew');
 		var data = get_product_form_data();
 		if(validate_product_form(data)){
 			if($('#AddProductForm').attr('table-row')){
@@ -19,8 +18,9 @@ $(document).ready(function(){
 					$('#ProductTable').append($('#ProductTable tbody tr:last').clone()).find('tbody tr:last').find('input,select,textarea').val('').fadeIn('slow');
 					var row =$('#ProductTable tbody tr:last').index();
 				}
-			}	
-			//upload_file();
+			}
+			ROW =  row;
+			upload_file();
 			populate_product_form_data(data,row);//POPULATE PRODUCT FORM DATA
 			update_row_index();//UPDATE ROW INDEX
 			reset_product_form();//RESET STUDENT PRODUCT FORM 
@@ -66,7 +66,7 @@ $(document).ready(function(){
 	//GET PRODUCT FORM DATA
 	function get_product_form_data(){
 		var data = [];
-		$.each($('#AddProductForm').find('input:visible,select:visible,textarea:visible'),function(i,o){
+		$.each($('#AddProductForm').find('input[type!="file"]:visible,select:visible,textarea:visible'),function(i,o){
 			array = { 
 				'id':$(o).attr('id'),
 				'field':$(o).attr('field'),
@@ -74,13 +74,14 @@ $(document).ready(function(){
 			}
 			data.push(array);
 		});
+		console.log(data);
 		return data;
 	}
 
 	//VALIDATE PRODUCT DATA
 	function validate_product_form(data){
 		for(var key in data){
-			//console.log(data[key]);
+			console.log(data[key]);
 			if(data[key].field == 'id'){
 				break;
 			}
@@ -118,7 +119,6 @@ $(document).ready(function(){
 	function populate_product_form_data(data,row){
 		$('#ProductTable tbody tr:eq('+row+')').fadeOut();
 		$.each(data,function(i,o){
-			//console.log(o.field +'-'+ o.value);
 			$('#ProductTable tbody tr:eq('+row+')').find('[field="'+o.field+'"]').val(o.value);
 		}); 
 		$('#ProductTable tbody tr:eq('+row+')').fadeIn().removeClass('onEdit').addClass('onSuccess');
@@ -130,8 +130,7 @@ $(document).ready(function(){
 		$('#AddProductForm').fadeOut();
 		$('#AddProductForm').attr('table-row',$(row).index());
 		$.each($(row).find('input,select,textarea'),function(i,o){
-			//console.log($(o).val());
-			$('#AddProductForm').find('[field="'+$(o).attr('field')+'"]').val($(o).val());	
+			$('#AddProductForm').find('[field="'+$(o).attr('field')+'"]').val($(o).val());
 		});
 		$('#AddProductForm').fadeIn();
 	}
@@ -140,9 +139,9 @@ $(document).ready(function(){
 	//RESET PRODUCT FORM
 	function reset_product_form(){
 		$('#AddProductForm').attr('table-row','').find('input,select,textarea').val('');
-		//$("#ProductLogoPath").fileinput('reset');
+		$("#ProductLogoPath").fileinput('reset');
 	}
-	/*
+
 	//INITIALIZE FILEINPUT UPLOADER
 	$("#ProductLogoPath").fileinput({
 		uploadAsync :false,
@@ -154,24 +153,40 @@ $(document).ready(function(){
 		 var form = data.form, files = data.files, extra = data.extra, 
 			response = data.response, reader = data.reader;
 			//Image urls are available on response.imageUrls
-			console.log(files,response);
-	});*/
+			var target = $('input[name="data[TemporaryRegistrationProduct]['+ROW+'][pictures]"]');
+			var pictures = target.val();
+			pictures = pictures.split(',');
+			target.val(pictures.concat(response.imageUrls).join(',')); //Combine pictures
+			var markup = '';
+			$.each(response.initialPreview,function(i,img){
+				var url = response.imageUrls[i];
+				markup+='<div class="thumbnail"><a aria-hidden="true" class="delete-picture pull-right" data-url="'+url+'" data-row="'+ROW+'">&times;</a>';
+				markup+=img;
+				markup+='</div>	';
+			});
+			$('#ProductTable tbody tr:nth-child('+(ROW+1)+') td.pictures').append(markup);
+	});
 	
 	//UPLOAD FILE
-	//function upload_file(){
-		//$("#ProductLogoPath").fileinput('upload');
-	//}
-	
-	setTimeout(function(){$('#flashMessage').remove()}, 6000);
-	
-	
-	$('.add-product-image').on('click',function(){
-		var row = $(this).parents('tr:first');
-		//console.log($(row).find('[field="name"]').val());
-	
-		$('#AddProductImageModal').modal();
-		$('#AddProductImageModal').find('.modal-title').text($(row).find('[field="name"]').val());
-		
+	function upload_file(){
+		$("#ProductLogoPath").fileinput('upload');
+	}
+	//DELETE PICTURE
+	$(document).on('click','a.delete-picture',function(){
+		var $this = $(this);
+		var url = $this.data('url');
+		var row = $this.data('row');
+		if(confirm('Are you sure you want to delete this picture?')){
+			var target = $('input[name="data[TemporaryRegistrationProduct]['+row+'][pictures]"]');
+			var pictures = [];
+			$.each(target.val().split(','),function(i,o){
+				if(o&&o!=url){
+					pictures.push(o);
+				}
+			});
+			target.val(pictures.join(','));
+			$this.parent().remove();			
+		}
 	});
-		
+	setTimeout(function(){$('#flashMessage').remove()}, 6000);
 });
